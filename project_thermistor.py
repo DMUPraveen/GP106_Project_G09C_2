c7ac4df769175ee93100f06b67bb260c103b7d34
DMUPraveen <dmu.praveen@gmail.com>
2022-02-16 13:22:11 +0530

    Updated project thermistor

--- project_thermistor.py
+++ project_thermistor.py
@@ -1,5 +1,6 @@
 # Importing libraries
 from pyfirmata import Arduino, util
+import Thermistor_Lib
 import time
 import math
 
@@ -13,7 +14,7 @@ iterator.start()
 #Assigning variables for the connected pins
 V = board.get_pin('a:0:i')
 B = board.get_pin('d:9:p')
-R1 = 1100 #fixed resistor value
+R1 = 980 #fixed resistor value
 
 #Steinhart constants for 1kOhm Thermistor
 c1 = 1.43291542079688e-3
@@ -22,23 +23,20 @@ c3 = -4.58846025754717e-7
 c4 = 1.87721342921284e-7
 
 
+
 while True:
     time.sleep(0.1)
     v = (V.read())
-    R2 =R1*(1/(v)-1) #Resistance of the Thermistor
-    #Steinhart equation to give temeperature read from the Thermistor 
-    T =  (1/(c1+c2*math.log(R2)+c3*math.log(R2)*math.log(R2)+c4*math.log(R2)*math.log(R2)*math.log(R2)))-273.15
+    print(v)
+    T = Thermistor_Lib.V_to_T(v,R1)
     print(T,'celcius')
 
 #conditioning whether to generate sound from the buzzer
-    if T > 10000:
-        B.write(0.2)
-        board.pass_time(0.5)
-        B.write(0.6)
-        board.pass_time(0.5)
-        B.write(0.8)
-        board.pass_time(0.5)
+    if T > 30:
+        B.write(1.0)
+    else:
         B.write(0)
+    
         
         
 
