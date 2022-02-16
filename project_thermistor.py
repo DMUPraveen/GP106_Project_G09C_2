e2a37093f2a1fc52343fd204c8749f358379c08d
Sahan9953 <99558521+Sahan9953@users.noreply.github.com>
2022-02-16 11:54:08 +0530

    Updated thermistor code (#7)

--- project_thermistor.py
+++ project_thermistor.py
@@ -10,26 +10,28 @@ board = Arduino('COM5')
 iterator = util.Iterator(board)
 iterator.start()
 
-#Assigning variables for the conncted pins
+#Assigning variables for the connected pins
 V = board.get_pin('a:0:i')
 B = board.get_pin('d:9:p')
 R1 = 1100 #fixed resistor value
 
-#Steinhart constants
-c1 = 1.009249522e-3
-c2 = 2.378405444e-4
-c3 = 2.019202697e-7
+#Steinhart constants for 1kOhm Thermistor
+c1 = 1.43291542079688e-3
+c2 = 2.72319937659526e-4
+c3 = -4.58846025754717e-7
+c4 = 1.87721342921284e-7
+
 
 while True:
     time.sleep(0.1)
     v = (V.read())
-    R2 =R1*(1.038/(v)-1) #Resistance of the thermistor
-    #Steinhart equation to give temeperature read from the thermistor
-    T =  (1/(c1+c2*math.log(R2*10)+c3*math.log(R2*10)*math.log(R2*10)*math.log(R2*10)))-273
+    R2 =R1*(1/(v)-1) #Resistance of the Thermistor
+    #Steinhart equation to give temeperature read from the Thermistor 
+    T =  (1/(c1+c2*math.log(R2)+c3*math.log(R2)*math.log(R2)+c4*math.log(R2)*math.log(R2)*math.log(R2)))-273.15
     print(T,'celcius')
 
 #conditioning whether to generate sound from the buzzer
-    if T > 30:
+    if T > 10000:
         B.write(0.2)
         board.pass_time(0.5)
         B.write(0.6)
@@ -37,3 +39,8 @@ while True:
         B.write(0.8)
         board.pass_time(0.5)
         B.write(0)
+        
+        
+
+
+
