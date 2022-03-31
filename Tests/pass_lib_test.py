d56fa36c875f7b809e0d78e9e66283ad302d94de
DMUPraveen <dmu.praveen@gmail.com>
2022-03-31 17:29:45 +0530

    Added package to handle passwrords

---
+++ Tests/pass_lib_test.py
@@ -0,0 +1,45 @@
+import test_config
+from pass_lib.pass_check import Password_Manager,hash_string
+import unittest
+
+class password_manager_tester(unittest.TestCase):
+
+    def setUp(self):
+        self.pass_dic = {
+            "user1" : hash_string("hello world"),
+            "user2" : hash_string("GP106"),
+            "user3" : hash_string("")
+        }
+        self.pm = Password_Manager(self.pass_dic)
+    def test_password_exists_and_valid(self):
+
+        re1 = self.pm.check_password_hash("user1","hello world")
+        self.assertTrue(re1.exists)
+        self.assertTrue(re1.valid)
+        re2 = self.pm.check_password_hash("user2","GP106")
+        self.assertTrue(re2.exists)
+        self.assertTrue(re2.valid)
+        re3 = self.pm.check_password_hash("user3","")
+        self.assertTrue(re3.exists)
+        self.assertTrue(re3.valid)
+    def test_password_exists_and_invalid(self):
+
+        re1 = self.pm.check_password_hash("user1","hello world ") # off by one spaec
+        self.assertTrue(re1.exists)
+        self.assertFalse(re1.valid)
+        re2 = self.pm.check_password_hash("user2","106GP")
+        self.assertTrue(re2.exists)
+        self.assertFalse(re2.valid)
+        re3 = self.pm.check_password_hash("user3","asd")
+        self.assertTrue(re3.exists)
+        self.assertFalse(re3.valid)
+    
+    def test_password_not_exist(self):
+        re1 = self.pm.check_password_hash("user","adadasd")
+        self.assertFalse(re1.exists)
+        self.assertIsNone(re1.valid)
+
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
---
+++ pass_lib/pass_check.py
@@ -0,0 +1,44 @@
+
+import hashlib
+import json
+import logging
+from tarfile import ENCODING
+from typing import Dict,Any,NamedTuple, Optional
+
+STRING_ENCODING = 'utf-8'
+
+
+
+
+def hash_string(string:str):
+    return hashlib.md5(bytes(string,'utf-8')).digest()
+
+class Password_Respones(NamedTuple):
+    exists : bool
+    valid : Optional[bool] = None
+
+class Password_Manager:
+    def __init__(self,dict:Dict[str,Any]):
+        self.passwords = dict
+                
+    def check_password_hash(self,user:str,password:str)->Password_Respones:
+        if(user not in self.passwords):
+            return Password_Respones(False)
+
+        true_hash = self.passwords[user]
+        pass_hash = hash_string(password)
+        if(pass_hash == true_hash):
+            return Password_Respones(True,True)
+    
+        return Password_Respones(True,False)
+            
+
+
+
+        
+
+
+
+
+
+
