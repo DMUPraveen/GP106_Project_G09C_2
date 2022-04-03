fb74523ee8ec24bec1294d07cdbb1e2907e52242
DMUPraveen <dmu.praveen@gmail.com>
2022-04-03 18:42:52 +0530

    Updated topics

--- Topics.py
+++ Topics.py
@@ -44,8 +44,8 @@ class PO:
 
     TEMPERATURE = _po("TEMPERATURE")
     SYS_ERR = _po("SYS_ERR")
-    KNOCK_SEND = _po("KNOCK_CODE")
-    KNOCK_ACCESS = _po("KNOCK_ACESS")
+    KNOCK_SEND = _po("KNOCK_SEND")
+    KNOCK_ACCESS = _po("KNOCK_ACCESS")
     ALARM_ON = _po("ALARM_ON")
     RAISE_ALARM = _po("ALARM_RAISE")
     PANIC_BUTTON = _po("PANIC_BUTTON")
